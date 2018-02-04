edff43e85472bdef43d1e71c207be808b93a1bdd
unknown <you@example.com>
2018-02-05 00:41:29 +0300

    new solution

--- index.php
+++ index.php
@@ -1,36 +1,43 @@
+<!DOCTYPE html>
 <html lang="ru">
   <head>
     <meta charset="utf-8">
     <title>Lesson-2</title>
-  </head> 
-</html>  
+    <style type="text/css">
+        .green { color: lightgreen; }
+    </style>
+  </head>
+  <body>
 
 <?php
-    $int = rand(0, 100);
-    echo "$int".'<br>';
+    // fibonacci
+    function func($int) {
+        $a = 1;
+        $b = 1;
 
-    $a = 1;
-    $b = 1;
-
-    while ($a < $int) {
-    	echo "Задуманное число НЕ входит в числовой ряд".'<br>';
-    	break;
+        while (true) {
+            if ($a > $int) {
+                echo "Задуманное число НЕ входит в числовой ряд".'<br>';
+                break;
+            }
+            if ($a == $int) {
+                echo "<span class='green'>Задуманное число входит в числовой ряд</span>";
+                break;
+            }
+            $c = $a;
+            $a = $a + $b;
+            $b = $c;
+        }
     }
 
-    while ($a == $int) {
-    	echo "Задуманное число входит в числовой ряд".'<br>';
-    	break;
-    }	
-
-    while ($a != $int) {
-    	$c = $a;
-    	#echo $c.'<br>';
-    	$a = $a + $b;
-    	#echo $a.'<br>';
-    	$b = "$c";
-    	#echo "$b".'<br>';
-        if ($a > 100)
-        break;
-    }   
-       
+    echo "<table><tr><th>Число</th><th>Входит в ряд?</th></tr>";
+    for ($i = 0; $i <= 100; $i++) {
+        echo "<tr><td>{$i}</td><td>";
+        func($i);
+        echo "</td></tr>";
+    }
+    echo "</table>";
 ?>
+
+    </body>
+</html>
